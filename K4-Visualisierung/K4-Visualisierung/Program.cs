a9683542c452da6b071a8f55a01299076fdac195
maagdaalenaa <s1410458015@students.fh-hagenberg.at>
2016-04-18 14:48:10 +0200

    kopieren dervisualisierung auf lokales repository

--- K4-Visualisierung/K4-Visualisierung/Program.cs
+++ K4-Visualisierung/K4-Visualisierung/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+using System.IO;
 
-namespace K4_Visualisierung
+namespace K4_Projekt
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UKH());
         }
     }
+
+    class stat_file
+    {
+        public static Dictionary<string, string> file_to_dictionary(string _file)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();//temporary dictionary
+            if (new FileInfo(_file).Exists)//checking if file exists
+            {
+                //Open file Stream
+                System.IO.StreamReader file = new System.IO.StreamReader(_file);
+
+                string support;
+
+                while ((support = file.ReadLine()) != null)//while not end of file
+                {
+                    if (support.Length != 0)//checking if file is not empty, only for important for the first iteration
+                    {
+                        var pair = support.Split(';');//splitt value pair
+                        dict.Add(pair[0], pair[1]);//add pair to dictionary
+                    }
+                    else
+                    {
+                        throw new Exception("File empty");
+                    }
+                }
+                file.Close();
+            }
+            else
+            {
+                throw new Exception("File not found");
+            }
+            return dict;
+        }
+    }
+
 }
