efca40861c88499d4390fad247e4308de444b1fe
AichP <aichinger.p@gmail.com>
2016-04-25 12:26:23 +0200

    runnallsamples pullen

---
+++ K4-Simulation/SimSharp.Samples/RunAllSamples.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimSharp.Samples
+{
+    class RunAllSamples
+    {
+        public static void Main()
+        {
+            Patient Patient1 = new Patient(400);
+
+            PatientGenerator catastrophe = new PatientGenerator(10);
+
+            Console.WriteLine(PatientGenerator.getNextPatient();
+            PatientGenerator.getNextPatient();
+            PatientGenerator.getNextPatient();
+        }
+    }
+}
