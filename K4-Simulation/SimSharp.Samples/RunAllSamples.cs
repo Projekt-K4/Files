fb9d99c0aae1380cd282a61cc527db012045805b
Andreas Haghofer <S1410458030@students.fh-hagenberg.at>
2016-05-03 15:32:43 +0200

    Added PatientManager class

--- K4-Simulation/SimSharp.Samples/RunAllSamples.cs
+++ K4-Simulation/SimSharp.Samples/RunAllSamples.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace SimSharp.Samples
 {
     class RunAllSamples
     {
         public static void Main(string[] args)
         {
-            eventLog.getLog();//for initalizing and creating file
-                       
-            new K4().RunSimulation(10);//Amount of Patients as Parameter
-            eventLog.getLog().writeToFile("C:/Users/Andreas/Desktop/log.csv");
-            eventLog.getLog().fromFileToConsole("C:/Users/Andreas/Desktop/log.csv", "\t");
+            ////patientManagerTest:
+            //Console.WriteLine("before:");
+            //List<Patient> before = patientManager.getInstance().createPatients(10);
+            //foreach(Patient p in before)
+            //{
+            //    Console.WriteLine(p.getTimeToLiveString());
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("after:");
+            //List<Patient> after = patientManager.getInstance().getAllPatients(true);
+            //foreach (Patient p in after)
+            //{
+            //    Console.WriteLine(p.getTimeToLiveString());
+            //}
+
+            //eventLog.getLog();//for initalizing and creating file
+
+            //new K4().RunSimulation(10);//Amount of Patients as Parameter
+            //eventLog.getLog().writeToFile("C:/Users/Andreas/Desktop/log.csv");
+            //eventLog.getLog().fromFileToConsole("C:/Users/Andreas/Desktop/log.csv", "\t");
             Console.WriteLine();
             Console.ReadKey();
         }
