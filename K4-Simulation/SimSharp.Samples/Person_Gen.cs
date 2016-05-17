a1bca12b5fc87e600a6f4e6a9c7ae5ff352412f1
AichP <aichinger.p@gmail.com>
2016-05-17 23:27:27 +0200

    Code Refactoring Commenting Distribution reworked

--- K4-Simulation/SimSharp.Samples/Person_Gen.cs
+++ K4-Simulation/SimSharp.Samples/Person_Gen.cs
@@ -6,49 +6,85 @@ using System.Text;
 
 namespace SimSharp.Samples
 {
-
+    //Definition of Patient status based on remaining TTL (in Seconds)
+   public static class TTLGlobal 
+    {
+        public const int TTL_DEAD = 0;                          //0 seconds left - dead
+        public const int TTL_HOPELESS = 3600;                   //3600 seconds left - hopeless
+        public const int TTL_SEVERELY_INJURED = 36000;          //36000 seconds left - severely injured
+        public const int TTL_SLIGHTLY_INJURED = 300000000;      //>36000 seconds left - slightly injured
+    }
 
     class PatientGenerator : IEnumerable, IEnumerator //PatientGenerator
     {
-        private static int sequence = 0; //Pool aus dem fortlaufende Katastrophen Nummer für Patient erstellt wird
+        /*          Fields          */
+        private static int sequence = 0;                                //iterating number for KID
         public SystemRandom globalTime = new SystemRandom();
-        private static List<Patient> patientList = new List<Patient>();
-        private static int position = -1;
-        private Random random_int = new Random();
+        private static List<Patient> patientList = new List<Patient>(); //List of all Patients
+        private static int position = -1;                               //position in List for Iterator
+        private Random randGen = new Random();                          //source for all randomness happening
 
+        //Distribution Array
+        //does not need to be given in percentages.Could also be set as:
+            // # {190, 150, 50, 60 }
+            // # {0.5 , 0.2, 12, 17} 
+        //Chance for generating:         Position:
+        private double[] distribution = { 0.25           // # dead Patient                  0
+                                        , 0.25           // # hopeless Patient              1
+                                        , 0.25           // # severly injured Patient       2
+                                        , 0.25 };        // # slightly injured Patient      3
 
+        /*          Constructor          */
         public PatientGenerator()
         {
-            Console.WriteLine("Catastrophe engages");
+            Console.WriteLine("Catastrophe engages\n");
 
         }
-        public PatientGenerator(int number_of_causualities)
+        //Creates PatientGenerator with given given number of causualities
+        public PatientGenerator(int _numberOfCausualities)
         {
-            generate_causualities(number_of_causualities);
-            Console.WriteLine(number_of_causualities + " involved in catastrophe");
+            generateCausualities(_numberOfCausualities);
+            Console.WriteLine(_numberOfCausualities + " involved in catastrophe\n");
         }
-        private void generate_causualities(int number_of_Patients)
+
+        /*          Private         */
+        //Produces given number of Patients and appends them to patientList
+        private void generateCausualities(int numberOfPatients)
         {
-            for (int i = 0; i < number_of_Patients; i++)
+            for (int i = 0; i < numberOfPatients; i++)
             {
-                add_Patient(patientList, get_random_time());
-                patientList.Add(new Patient(get_random_int(0,11), get_random_int(0, 59), get_random_int(0, 59)));
+                addPatient();
             }
         }
-        public int get_random_int(int min, int max)
-        {
-            
-            return random_int.Next(min,max);
-        }
+        /*          Public          */
 
-        public TimeSpan get_random_time()
+        /*public TimeSpan get_random_time()
         {
             TimeSpan random_time = new TimeSpan(globalTime.Next(0,12));
             return random_time;
-        }
-        public bool add_Patient(List<Patient> plist, TimeSpan random_time)
+        }*/
+        
+         //Adds a new Patient to the List according to the given distribution
+        public bool addPatient()
         {
-            plist.Add(new Patient(get_random_int(0,11), get_random_int(0,59), get_random_int(0, 59)));
+            double x = randGen.NextDouble();            // determins the Patients state
+            double sumDist = distribution.Sum();        
+
+            if      (x <= distribution[0]/sumDist)
+                patientList.Add(new Patient(TTLGlobal.TTL_DEAD));
+
+            else if (x <= (distribution[0] + distribution[1])/sumDist)
+                patientList.Add(new Patient((double)randGen.Next(TTLGlobal.TTL_DEAD + 1, TTLGlobal.TTL_HOPELESS))); // Creates hopeless patient as distributed
+
+            else if (x <= (distribution[0] + distribution[1] + distribution[2])/sumDist)
+                patientList.Add(new Patient((double)randGen.Next(TTLGlobal.TTL_HOPELESS + 1, TTLGlobal.TTL_SEVERELY_INJURED)));
+
+            else if (x <= distribution.Sum())
+                patientList.Add(new Patient((double)randGen.Next(TTLGlobal.TTL_SEVERELY_INJURED + 1, TTLGlobal.TTL_SLIGHTLY_INJURED)));
+
+            else
+                Console.WriteLine("Something went wrong!");
+         
             return true;
         }
 
@@ -95,38 +131,39 @@ namespace SimSharp.Samples
         {
             return patientList;
         }
+
+        //changes the distribution
+        public void setDistribution(double dead, double hopeless, double severe, double slight)
+        {
+            distribution[0] = dead;
+            distribution[1] = hopeless;
+            distribution[2] = severe;
+            distribution[3] = slight;
+        }
     }
 
 
     class Patient
     {
-        /* public string KID;
-         public DateTime waitingTime;
-         public DateTime TTL;
-         public DateTime arrivalTime;*/
-        private int KID = PatientGenerator.get_new_id();
+        
+        private string KID = "4 KH 12 " + PatientGenerator.get_new_id();
         public DateTime arrivalTime;
         private DateTime TTL = new DateTime(); //Is the time the Patient has left to live
         private int triageNr;
-        //public Patient()
-        //{
-        //    Console.WriteLine("Patient with default life expectancy(600) created");
-        //}
-        //public Patient(DateTime assignTime) //Overloaded Constructor
-        //{
-        //    TTL = assignTime;
-        //    Console.WriteLine("Patient with life expectancy:" + TTL + "created");
-        //}
-        public Patient(int hours, int minutes, int seconds) //Overloaded Constructor
+        
+       
+        //When creating the patient, TTL will be set to the current time + the given seconds
+        public Patient(double seconds) //Overloaded Constructor
         {
-            DateTime thisDay = DateTime.Now;
-            setTimeToLive(new DateTime(thisDay.Year, thisDay.Month, thisDay.Day, hours, minutes, seconds));
+            DateTime thisDay = DateTime.Now.AddSeconds(seconds);
+            setTimeToLive(new DateTime(thisDay.Year, thisDay.Month, thisDay.Day, thisDay.Hour, thisDay.Minute, thisDay.Second));
             //Console.WriteLine("Patient with life expectancy:" + TTL + "created");
+            //thisDay.AddSeconds(150);
         }
 
         public String getKID()
         {
-            return "4 KH 12 "+KID;
+            return KID;
         }
 
         public DateTime getTimeToLive()
@@ -159,9 +196,9 @@ namespace SimSharp.Samples
         {
             
             int TTLInSec =TTL.Hour*3600 + TTL.Minute*60 + TTL.Second;
-            if (TTLInSec <= 0) triageNr = 4; //dead
-            else if (TTLInSec > 0 && TTLInSec <= 3600) triageNr = 3; //TTL <= 60min -> hopeless
-            else if (TTLInSec > 3600 && TTLInSec <= 36000) triageNr = 2; //TTL > 15min and <= 10h -> serverely injured
+            if (TTLInSec <= TTLGlobal.TTL_DEAD) triageNr = 4; //dead
+            else if (TTLInSec > TTLGlobal.TTL_DEAD && TTLInSec <= TTLGlobal.TTL_HOPELESS) triageNr = 3; //TTL <= 60min -> hopeless
+            else if (TTLInSec > TTLGlobal.TTL_HOPELESS && TTLInSec <= TTLGlobal.TTL_SEVERELY_INJURED) triageNr = 2; //TTL > 15min and <= 10h -> serverely injured
             else triageNr = 1; //TTL > 10h -> slightly injured
 
         }
@@ -179,7 +216,7 @@ class Program
     {
         static void Main()
         {
-           // Patient Patient1 = new Patient();
+            Patient Patient1 = new Patient(100);
 
             //PatientGenerator catastrophe = new PatientGenerator(10);
 
