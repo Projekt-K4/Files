f0a2c178a2d064e72d8a6513918f71d9317aa7e8
Andreas Haghofer <S1410458030@students.fh-hagenberg.at>
2016-06-13 13:16:32 +0200

    refactoring

--- K4-Simulation/SimSharp.Samples/K4.cs
+++ K4-Simulation/SimSharp.Samples/K4.cs
@@ -100,7 +100,7 @@ namespace SimSharp.Samples
                 eventLog.getLog().addLog(env.Now.ToLongTimeString(), pat.getTimeToLiveString(), pat.getKID(), pat.getTriageNr().ToString(), "12");
 
                 //OP Resources:
-                var op = RSStore.getInstance().OPStore.Get();
+                OP op = RSStore.getInstance().GetOP();
                 var ch = RSStore.getInstance().ChirurgStore.Get();
                 var ane = RSStore.getInstance().AnesthesistStore.Get();
                 var nurse1 = RSStore.getInstance().NurseStore.Get();
@@ -108,24 +108,23 @@ namespace SimSharp.Samples
                 var anen = RSStore.getInstance().AnesthesistNurseStore.Get();
                 var support = RSStore.getInstance().SupportStore.Get();
                 var rta = RSStore.getInstance().RTAStore.Get();
-
-             
+                
                 yield return ch;
-                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "1" + op.Value);
+                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "1" + op);
                 yield return ane;
-                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "5" + op.Value);
+                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "5" + op);
                 yield return nurse1;
-                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "2" + op.Value);
+                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "2" + op);
                 yield return nurse2;
-                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "3" + op.Value);
+                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "3" + op);
                 yield return anen;
-                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "6" + op.Value);
+                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "6" + op);
                 yield return support;
-                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "4" + op.Value);
+                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "4" + op);
                 yield return rta;
-                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "7" + op.Value);
+                eventLog.getLog().addLog(env.Now.ToLongTimeString(), "", "", "", "7" + "7" + op);
                 yield return op;
-                eventLog.getLog().addLog(env.Now.ToLongTimeString(), pat.getTimeToLiveString(), pat.getKID(), pat.getTriageNr().ToString(), "4" + op.Value);
+                eventLog.getLog().addLog(env.Now.ToLongTimeString(), pat.getTimeToLiveString(), pat.getKID(), pat.getTriageNr().ToString(), "4" + op);
                 //System for new TTL if patient dies or survives the OP Process!!!!
                 //Has to be changed in the near future!!!!!!!!
                 if (survive(env, pat))
@@ -137,7 +136,7 @@ namespace SimSharp.Samples
                 //re triage process
                 env.Process(Triage(env, pat, 4));
                 // env.Process(WardProcess(env, pat, Mortuary, op.Value.ToString()));
-                RSStore.getInstance().OPStore.Put(op.Value); RSStore.getInstance().ChirurgStore.Put(ch.Value); RSStore.getInstance().AnesthesistStore.Put(ane.Value); RSStore.getInstance().NurseStore.Put(nurse1.Value); RSStore.getInstance().NurseStore.Put(nurse2.Value); RSStore.getInstance().AnesthesistNurseStore.Put(anen.Value); RSStore.getInstance().SupportStore.Put(support.Value);
+                RSStore.getInstance().PutOP(op); RSStore.getInstance().ChirurgStore.Put(ch.Value); RSStore.getInstance().AnesthesistStore.Put(ane.Value); RSStore.getInstance().NurseStore.Put(nurse1.Value); RSStore.getInstance().NurseStore.Put(nurse2.Value); RSStore.getInstance().AnesthesistNurseStore.Put(anen.Value); RSStore.getInstance().SupportStore.Put(support.Value);
                 // Ops.Put(new OP(obj.Value.ToString()));
             }
 
